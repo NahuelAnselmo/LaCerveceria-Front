87f5d5279bf93a676934b3159264748d1ffc0e6e
Mariojar96 <maritoodk@gmail.com>
2024-10-29 20:53:41 -0300

    Modifico componente menu para que actualice stock despues de confirmar compra

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,6 +1,23 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 
+// Función para actualizar el stock de un producto
+export const updateStockFn = async (productId, quantity) => {
+    const response = await fetch(`${BACKEND_URL}/products/${productId}/update-stock`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ quantity }),
+    });
+  
+    if (!response.ok) {
+      throw new Error("Error al actualizar el stock");
+    }
+  
+    return response.json();
+  };
+
 export const getMenuFn = async () => {
     const res= await fetch (`${BACKEND_URL}/products`);
     const data = await res.json();
