36269113a62cf03783c23843a620f310419a4dc1
Mariojar96 <maritoodk@gmail.com>
2024-12-10 20:03:40 -0300

    Se quita número de mesa de footerNavbar

--- src/components/FooterNavbar/FooterNavBar.jsx
+++ src/components/FooterNavbar/FooterNavBar.jsx
@@ -4,20 +4,13 @@ import PropTypes from "prop-types";
 import "../FooterNavbar/footerNavBar.css"
 
 
-const FooterNavbar = ({ totalAmount, onCartClick,tableNumber  }) => {
+const FooterNavbar = ({ totalAmount, onCartClick  }) => {
     return (
       <div className="navbar-footer">
-      <div className={`content-footer px-2 d-flex ${tableNumber ? 'justify-content-between' : 'justify-content-start'} align-items-center`}>
+      <div className="content-footer px-2 d-flex justify-content-start align-items-center">
         <span className="item-footer">
           Total: <strong>${totalAmount.toFixed(2)}</strong>
-        </span>
-        
-        {tableNumber && (
-          <span className="item-footer">
-            Mesa: <strong>{tableNumber}</strong>
-          </span>
-        )}
-        
+        </span>        
         <div className="footer-icon ms-auto" onClick={onCartClick}>
           <FontAwesomeIcon icon={faShoppingCart} />
         </div>
@@ -31,5 +24,5 @@ const FooterNavbar = ({ totalAmount, onCartClick,tableNumber  }) => {
     FooterNavbar.propTypes = {
       totalAmount: PropTypes.number.isRequired,
       onCartClick: PropTypes.func.isRequired,  
-      tableNumber: PropTypes.string.isRequired
+
     };
